b036329f63c3e885cf338702ea4e414b5045fdf5
GusTavo <48064893+estudantedosenai@users.noreply.github.com>
2019-08-15 16:24:00 -0300

    closse #15 envio de dados para o back-end

---
+++ recebe.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  back-end
+ * aula04 15/08/2019 - Recepção de dados de
+ */
+#Backend
+
+echo"<pre>/n";
+print_r($_POST);
+echo"\n</pre>\n";
--- rescebe.php
+++
@@ -1,9 +0,0 @@
-<?php 
-#Backend
-
-echo "<pre>\n";
-print_r($_get);
-echo "</pre>\n";
-
-echo "\n<h1>Nome: $_GET[usuario]</h1>";
-echo "\n<h2>senha: $_GET[senha]</h2>";
