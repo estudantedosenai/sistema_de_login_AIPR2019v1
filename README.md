880a9df4e524d13601e9d1c3946f2a8e1b96723a
GusTavo <gustavo_bueno@estudante.sc.senai.br>
2019-08-23 16:44:52 -0300

    recepção de dados para cadastro de usuário

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
 
+## aula05 - 23/08/2019
+criação de banco de dados e ~cadastro de usuários~
+
 ## aulo04 - 15/08/2019
 recepção de dados no back-end
 
--- recebe.php
+++ recebe.php
@@ -1,10 +1,43 @@
 <?php
-/**
- *  back-end
- * aula04 15/08/2019 - Recepção de dados de
- */
-#Backend
 
-echo"<pre>/n";
-print_r($_POST);
-echo"\n</pre>\n";
+// É necessario fazer a coneção com banco de dados
+require_once "configDB.php";
+
+function verificar_entrada($entrada){
+    $saida = trim($entrada); // remove espaços antes e depois 
+    $saida = stripslashes($saida); // remove as baras
+    $saida = htmlspecialchars($saida);
+    return $saida;
+}
+
+if (isset($_POST['action']) && $_POST['action'] = 'cadastro') {
+    $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+    $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
+    $emailUsuario = verificar_entrada($_POST['emailUsuário']);
+    $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
+    $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
+    $concordar = $_POST['concordar'];
+    $dataCriacao = date("y-m-d H:i:s");
+  
+
+    echo "<h5>Nome completo: $nomeCompleto</h5>";
+    echo "<h5>Nome Usuário: $nomeUsuario</h5>";
+    echo "<h5>E-mail Usuário: $emailUsuario</h5>";
+    echo "<h5>senha Confirma: $senhaConfirma</h5>";
+    echo "<h5>Concordar: $concordar</h5>";
+    echo "<h5>data de criação: $dataCriacao</h5>";
+
+    //Hash de senha / codificação de senha em 40 caracteres
+    $senha = sha1($senhaUsuario);
+    $senhaC = sha1($senhaConfirma);
+
+    if($senha != $senhaC){
+        echo "<h1>As senhas não conferem</h1>";
+        exit();
+    }else{
+        echo "<h5> senha codificada: $senha</h5>";
+    }
+
+} else {
+    echo "<h1 style='color:red'>Esta Página não pode ser acessada diretamente</h1>";
+}
