5944070e78462c6e636c562a35cd3f04a0d30bbc
GusTavo <gustavo_bueno@estudante.sc.senai.br>
2019-08-30 16:31:18 -0300

    Cadastro de usuário
    closes #8

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
 
+## AULA06 - 30/08/2019
+Ùltima sexta-feira de agosto de 2019!
+Cadastro de usuário.
+
 ## aula05 - 23/08/2019
 criação de banco de dados e ~cadastro de usuários~
 
--- recebe.php
+++ recebe.php
@@ -18,14 +18,6 @@ if (isset($_POST['action']) && $_POST['action'] = 'cadastro') {
     $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
     $concordar = $_POST['concordar'];
     $dataCriacao = date("y-m-d H:i:s");
-  
-
-    echo "<h5>Nome completo: $nomeCompleto</h5>";
-    echo "<h5>Nome Usuário: $nomeUsuario</h5>";
-    echo "<h5>E-mail Usuário: $emailUsuario</h5>";
-    echo "<h5>senha Confirma: $senhaConfirma</h5>";
-    echo "<h5>Concordar: $concordar</h5>";
-    echo "<h5>data de criação: $dataCriacao</h5>";
 
     //Hash de senha / codificação de senha em 40 caracteres
     $senha = sha1($senhaUsuario);
@@ -35,9 +27,29 @@ if (isset($_POST['action']) && $_POST['action'] = 'cadastro') {
         echo "<h1>As senhas não conferem</h1>";
         exit();
     }else{
-        echo "<h5> senha codificada: $senha</h5>";
+        //echo "<h5> senha codificada: $senha</h5>";
+        //Verifiacr se o usuario já existe no banco de dados
+        $sql = $conecta->prepare("SELECT nomeUsuario, email FROM usuario WHERE nomeUsuario = ? OR email = ?");
+        //substituir cada ? por string abaixo
+        $sql->bind_param("ss",$nomeUsuario, $emailUsuario);
+        $sql->execute();
+        $resultado = $sql->get_result();
+        $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+        if($linha['nomeUsuario']== $nomeUsuario){
+            echo "<p>Nomen de usuário indisponivel, tente outro</p>";
+        }elseif ($linha['email'] == $emailUsuario){
+            echo "<p>E-mail já em uso, tente outro</p>";
+        }else{
+            $sql = $conecta->prepare("INSERT INTO usuario (nome, nomeUsuario, email, senha, dataCriacao)values(?, ?, ?, ?, ?)");
+            $sql->bind_param("sssss",$nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $dataCriacao);
+            if($sql->execute()){
+                echo "<p>Registrado com sucesso</p>";
+            }else {
+                echo "<p>Algo deu errado. Tente outra vez</p>";
+            }
+        } 
+ 
     }
-
 } else {
     echo "<h1 style='color:red'>Esta Página não pode ser acessada diretamente</h1>";
 }
