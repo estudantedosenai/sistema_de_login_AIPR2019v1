add32ddc3d858f97e137b46dd6b7d665dcb890a2
GusTavo <gustavo_bueno@estudante.sc.senai.br>
2019-09-12 16:39:44 -0300

    preparação para gerar nova senha

--- gerar_senha.php
+++ gerar_senha.php
@@ -20,18 +20,30 @@
                     Crie uma nova senha
                 </h3>
                 <h4 class="text-center">
-                    <?= @$msg ?> 
+                    <?= @$msg ?>
                 </h4>
                 <form action="" method="post">
                     <div class="form-group">
-                        <input type="password" name="senha" id="senha" class="form contrel placeholder=""nova senha " requiserd>
-
+                        <label for="senha">
+                            Nova Senha
+                        </label>
+                        <input type="password" name="senha" id="senha" class="form-control" placeholder="Nova Senha" required>
                     </div>
+                    <div class="form-group">
+                        <label for="csenha">
+                            Confirme a Senha
+                        </label>
+                        <input type="password" name="csenha" id="csenha" class="form-control" placeholder="Nova Senha" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" value=":: Criar nova Senha ::" name="criar" class="btn btn-success btn-block" style="background: purple; color: white; font-weight: bolder; padding: 10px; font-size: 22px; box-shadow: 3px 3px 5px black;">
+                    </div>
+
                 </form>
             </div>
         </section>
     </main>
-       <!-- Optional JavaScript -->
+    <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
